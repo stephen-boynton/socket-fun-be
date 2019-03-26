0422700c0abd2d6541845a192e0652af9403b9e3
Stephen Boynton <stephen.boynton@homes.com>
2019-03-26 13:28:35 -0400

    figuring out structure and passing state

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
 import express from 'express';
 import socket from 'socket.io';
+import uuid = require('uuid');
 
 const app = express();
 const server = require('http').Server(app);
 const io = socket(server);
 
-// var app = require('express')();
-// var server = require('http').Server(app);
-// var io = require('socket.io')(server);
-
 server.listen(3001, () => {
   console.log('Server is on...');
 });
-// WARNING: app.listen(80) will NOT work here!
 
 app.get('/', async (req, res) => {
   await res.send('hello');
 });
 
 io.on('connection', (socket) => {
-  console.log('what up what up')
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  console.log('connected', socket.id);
+  socket.on('host', () => {
+    const newRoom: string = uuid.v4();
+    socket.emit('joined', newRoom);
+    socket.join(newRoom);
   });
+
+  socket.on('join', () => {
+    console.log(io.sockets.adapter.rooms);
+  })
 });
